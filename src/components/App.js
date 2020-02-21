97d6f13c145e821c7e1a8feb756ab85dca9f26e1
Donald Fries <donald.darklight@gmail.com>
2020-02-21 13:39:35 -0600

    added map generation/display

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { generateDungeon } from '../lib/map';
 
 // Take Home Projects - Build a Roguelike Dungeon Crawler Game
 // Objective: Build a CodePen.io app that is functionally similar to this: https://codepen.io/freeCodeCamp/full/apLXEJ/.
@@ -14,10 +15,70 @@ import React from 'react';
 // User Story: The game should be challenging, but theoretically winnable.
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.onResize = this.onResize.bind(this);
+    this.onAnimationFrame = this.onAnimationFrame.bind(this);
+    this.onClick = this.onClick.bind(this);
+    this.state = {
+      map: generateDungeon(101, 53, 1000)
+    };
+  }
+
+  componentDidMount() {
+    this._resizeViewport();
+    window.addEventListener('resize', this.onResize);
+    this.frame = window.requestAnimationFrame(this.onAnimationFrame);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('resize', this.onResize);
+    window.cancelAnimationFrame(this.frame);
+  }
+
+  onResize() {
+    this._resizeViewport();
+  }
+
+  onAnimationFrame() {
+    this._drawMap();
+    this.frame = window.requestAnimationFrame(this.onAnimationFrame);
+  }
+
+  onClick() {
+    this.setState({
+      map: generateDungeon(101, 53, 1000)
+    });
+  }
+
+  _resizeViewport() {
+    const canvas = this.canvas;
+    const rect = canvas.getBoundingClientRect()
+    canvas.width = rect.width;
+    canvas.height = rect.height;
+  }
+
+  _drawMap() {
+    const { width, height, data } = this.state.map;
+    const ctx = this.canvas.getContext('2d');
+    ctx.fillStyle = '#FFFFFF';
+    ctx.fillRect(0, 0, this.canvas.width, this.canvas.height);
+    ctx.fillStyle = '#000000';
+    const cellWidth = Math.floor(this.canvas.width / width);
+    const cellHeight = Math.floor(this.canvas.height / height);
+    for (let y = 0; y < height; y++) {
+      for (let x = 0; x < width; x++) {
+        if (data[y][x] !== 1) {
+          ctx.fillRect(x * cellWidth, y * cellHeight, cellWidth, cellHeight);
+        }
+      }
+    }
+  }
+
   render() {
     return (
       <div className="screen">
-        <canvas id="canvas" ref={elem => this.canvas = elem}></canvas>
+        <canvas id="canvas" ref={elem => this.canvas = elem} onClick={this.onClick}></canvas>
       </div>
     );
   }
